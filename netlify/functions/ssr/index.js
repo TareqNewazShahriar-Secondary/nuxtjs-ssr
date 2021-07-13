303658394d4508662e8b3f0c0a786d2cb3d071c0
tareq.newaz.shahriar <tareq.newaz.shahriar@gmail.com>
2021-07-13 08:59:18 +0600

    netlify function

--- netlify/functions/ssr/index.js
+++ netlify/functions/ssr/index.js
@@ -1,21 +1,35 @@
 const {Nuxt} = require('nuxt');
 
+let _event;
+let _context;
+
 exports.handler = async function(event, context) {
+   _event = event;
+   _context = context;
+
    const nuxtConfig = {
       dev: false,
       buildDir: 'nuxt'
    }
-   const nuxt = new Nuxt(nuxtConfig);
 
-   return new Promise((resolve, reject) => {
-      nuxt.renderRoute('/')
-         .then(result => {
-            resolve({
-               statusCode: 200,
-               headers: { 'Content-Type': 'text/html', 'x-explore': `${__dirname} ||| ${Object.keys(event).join(' / ')} ||| ${Object.keys(context).join(' / ')}` },
-               body: result.html
-            });
-         })
-         .catch(err => reject(err));
-   });
+   try {
+      const nuxt = new Nuxt(nuxtConfig);
+
+      return new Promise((resolve, reject) => {
+         nuxt.renderRoute('/')
+            .then(result => resolve(responseObject(result.html)))
+            .catch(err => reject(responseObject(err)));
+      });
+   }
+   catch(err) {
+      return responseObject(`${err.toString()} ||| ${Object.entries(Nuxt).join(' // ')} ||| ${Object.entries(nuxt).join(' // ')}`);
+   }
+}
+
+function responseObject(result) {
+   return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'text/html', 'x-explore': `${__dirname} ||| ${Object.entries(_event).join(' / ')} ||| ${Object.entries(_context).join(' / ')}` },
+      body: result
+   };
 }
